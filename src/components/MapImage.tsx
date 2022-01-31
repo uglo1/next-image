a18da3047d1573041dcba73dfb2727ac89d81dc1
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-31 16:14:00 +0900

    ♻️

--- src/components/MapImage.tsx
+++ src/components/MapImage.tsx
@@ -1,8 +1,49 @@
 import Image from "next/image";
+import styles from "src/components/MapImage.module.css";
+import PinDropIcon from "@material-ui/icons/PinDrop";
 
 export const MapImage = (props: any) => {
+  const data = [
+    {
+      id: 1,
+      name: "上落合",
+      x: 47.3,
+      y: 60,
+      color: "red",
+    },
+    {
+      id: 2,
+      name: "###",
+      x: 40.7,
+      y: 50,
+      color: "blue",
+    },
+    {
+      id: 3,
+      name: "###",
+      x: 53.5,
+      y: 25,
+      color: "yellow",
+    },
+    {
+      id: 4,
+      name: "###",
+      x: 45,
+      y: 32.5,
+      color: "green",
+    },
+    {
+      id: 5,
+      name: "###",
+      x: 56,
+      y: 51,
+      color: "orange",
+    },
+  ];
+
   return (
-    <>
+    <div className={styles.map}>
+      {/* 画像 */}
       <Image
         src="/map.png"
         alt="map"
@@ -10,6 +51,22 @@ export const MapImage = (props: any) => {
         width={600}
         height={300}
       />
-    </>
+
+      {/* ピンのデータ */}
+      {data.map((loc) => {
+        return (
+          <div
+            key={loc.id}
+            style={{
+              position: "absolute",
+              left: `${loc.x}%`,
+              bottom: `${loc.y}%`,
+            }}
+          >
+            <PinDropIcon htmlColor={loc.color} />
+          </div>
+        );
+      })}
+    </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,13 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { MapImage } from "src/components/MapImage";
-import PinDropIcon from "@material-ui/icons/PinDrop";
 
 const Dashboard: NextPage = () => {
   return (
     <>
       <h1>Hello World</h1>
       <div className={styles.wrapper}>
-        <div className={styles.side}>
-          <MapImage />
-          <div className={styles.pin1}>
-            <PinDropIcon htmlColor="red" />
-          </div>
-          <div className={styles.pin2}>
-            <PinDropIcon htmlColor="blue" fontSize="large" />
-          </div>
-          <div className={styles.pin3}>
-            <PinDropIcon htmlColor="yellow" />
-          </div>
-          <div className={styles.pin4}>
-            <PinDropIcon htmlColor="green" />
-          </div>
-          <div className={styles.pin5}>
-            <PinDropIcon htmlColor="orange" />
-          </div>
-        </div>
+        <MapImage />
       </div>
     </>
   );
